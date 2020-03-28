c1a97928eab69270b8ba92f6b99e409bbca02b21
Dmitry <dmitry.shpagin@balance-pl.ru>
2020-03-28 17:31:23 +0300

    version up

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pillar.MixProject do
   def project do
     [
       app: :pillar,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
