e58a7ad0ca2d3b87a2d15181168023e2031a816f
Dmitry Shpagin <dmitry.shpagin@balance-pl.ru>
2022-09-25 14:55:28 +0300

    Little fixups

--- test/pillar_test.exs
+++ test/pillar_test.exs
@@ -610,7 +610,7 @@ defmodule PillarTest do
                   %{"field4" => [{:baz, "bak"}, {:foo, "bar"}]}
                 ]} = Pillar.select(conn, "select * from #{table_name}")
       else
-        Logger.warn("insert keyword as map, becouse CH major version is lower than 21")
+        Logger.warn("insert keyword as map, because CH major version is lower than 21")
       end
     end
 
@@ -618,7 +618,7 @@ defmodule PillarTest do
       %{"major" => major, "minor" => minor} = version(conn)
 
       if major > 21 or (major >= 21 and minor >= 12) do
-        table_name = "to_table_inserts_keyword_#{@timestamp}"
+        table_name = "to_table_inserts_booleans_#{@timestamp}"
 
         create_table_sql = """
         CREATE TABLE IF NOT EXISTS #{table_name} (
@@ -628,7 +628,7 @@ defmodule PillarTest do
 
         assert {:ok, ""} = Pillar.query(conn, create_table_sql)
 
-        record = %{"field5" => true}
+        record = [%{"field5" => true}]
 
         assert {:ok, ""} = Pillar.insert_to_table(conn, table_name, record)
 
